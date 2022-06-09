4dcc8d8321ff2d25cc3202312d492ea67e7226e2
Gabriel Salazar <gsaalcantara@gmail.com>
2022-06-09 11:55:48 -0300

    Criação da Controller/View Vendedores

---
+++ TrabalhoMVC/Controllers/VendedoresController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabalhoMVC.Controllers {
+	public class VendedoresController : Controller {
+		public IActionResult Index() {
+
+			return View();
+		}
+	}
+}
