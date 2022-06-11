7c3de8c6ecdfb789b8df247406099efd7986cbec
Gabriel Salazar <gsaalcantara@gmail.com>
2022-06-11 11:03:41 -0300

    VendedorService criado, Tela Index de Vendedores mostra todos os Vendedores cadastrados

--- TrabalhoMVC/Controllers/VendedoresController.cs
+++ TrabalhoMVC/Controllers/VendedoresController.cs
@@ -3,12 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TrabalhoMVC.Services;
 
 namespace TrabalhoMVC.Controllers {
 	public class VendedoresController : Controller {
-		public IActionResult Index() {
 
-			return View();
+		private readonly VendedorService _sellerService;
+
+		public VendedoresController(VendedorService sellerService) {
+
+			_sellerService = sellerService;
+
+		}
+		public IActionResult Index() {
+			var list = _sellerService.FindAll();
+
+			return View(list);
 		}
 	}
 }
--- TrabalhoMVC/Startup.cs
+++ TrabalhoMVC/Startup.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TrabalhoMVC.Data;
+using TrabalhoMVC.Services;
 
 namespace TrabalhoMVC
 {
@@ -32,6 +33,7 @@ namespace TrabalhoMVC
 					builder.MigrationsAssembly("TrabalhoMVC")));
 
 			services.AddScoped<SeedingService>();
+			services.AddScoped<VendedorService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
