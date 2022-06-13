b2bb0d58bb5e07d235580d21eec4f5d534545bd6
Gabriel Salazar <gsaalcantara@gmail.com>
2022-06-13 10:08:21 -0300

    Busca Simples implementada

--- TrabalhoMVC/Controllers/RegistroVendasController.cs
+++ TrabalhoMVC/Controllers/RegistroVendasController.cs
@@ -3,14 +3,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TrabalhoMVC.Services;
 
 namespace TrabalhoMVC.Controllers {
 	public class RegistroVendasController : Controller {
+
+		private readonly RegistroVendasService _registroVendasService;
+
+		public RegistroVendasController(RegistroVendasService registroVendasService) {
+			_registroVendasService = registroVendasService;
+		}
+
 		public IActionResult Index() {
 			return View();
 		}
-		public IActionResult SimpleSearch() {
-			return View();
+		public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate) {
+
+			if (!minDate.HasValue) {
+
+				minDate = new DateTime(DateTime.Now.Year, 1, 1);
+			}
+			if (!maxDate.HasValue) {
+
+				maxDate = DateTime.Now;
+			}
+			ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+			ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+			var result = await _registroVendasService.FindByDateAsync(minDate, maxDate);
+			return View(result);
 		}
 		public IActionResult GroupingSearch() {
 			return View();
--- TrabalhoMVC/Startup.cs
+++ TrabalhoMVC/Startup.cs
@@ -37,6 +37,7 @@ namespace TrabalhoMVC
 			services.AddScoped<SeedingService>();
 			services.AddScoped<VendedorService>();
 			services.AddScoped<DepartamentoService>();
+			services.AddScoped<RegistroVendasService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
